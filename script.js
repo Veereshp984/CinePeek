0f123d95816c30eace6bd128355ad33fb447bbb7
veeresh <pasareveeresh0908@gmail.com>
2025-11-22 00:04:40 +0530

    added result section

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let btn = document.querySelector('#searchBtn');
 search.addEventListener('input', () => {
     console.log(search.value);
 })
+
+
+
+let p = fetch('http://www.omdbapi.com/?i=tt3896198&apikey=cc66c655');
+p.then((Response)=>{
+    return Response.json();
+}).then((data)=>{
+    console.log(data);
+});
