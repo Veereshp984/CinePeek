e8a5f98d099ceab6db12f003892dd0712f7d8af6
veeresh <pasareveeresh0908@gmail.com>
2025-11-21 22:27:31 +0530

    done with the basic structureand styling

---
+++ script.js
@@ -0,0 +1,6 @@
+let search = document.querySelector('.search input');
+let btn = document.querySelector('#searchBtn');
+
+search.addEventListener('input', () => {
+    console.log(search.value);
+})
